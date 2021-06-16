9d5631987892a1038ab1a774f8d3e720fe7cca02
no8ter <rudovskii5@mail.ru>
2021-06-16 08:18:01 +0300

    Add start.bat

---
+++ start.bat
@@ -0,0 +1,11 @@
+
+
+IF NOT EXIST "venv\Scripts\pip.exe" (
+	python -m venv venv 
+	"venv\Scripts\pip" install -r requirements.txt
+) ELSE (
+	echo venv exists already.
+)
+
+"venv\Scripts\python.exe" wsgi.py
+exit
